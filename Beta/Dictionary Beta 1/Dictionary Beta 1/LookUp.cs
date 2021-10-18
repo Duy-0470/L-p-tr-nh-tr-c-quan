b6e884a0db1aee4b51ee1caa56cc4ab47543940a
Ecstacyber <duyhao301202@gmail.com>
2021-10-18 10:25:03 +0700

    Dictionary v0.1

---
+++ Beta/Dictionary Beta 1/Dictionary Beta 1/LookUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary_Beta_1
+{
+    public partial class LookUp : Form
+    {
+        public static LookUp instance;
+        public string[] lines = System.IO.File.ReadAllLines(@"E:\VS Studio\Year 2\Visual Programming\Dictionary Betas\Dictionary Beta 1\Dictionary Beta 1\CSDL.txt");
+        public TextBox tb;
+        private readonly WordDatabase WordDB = WordDatabase.Instance;
+        public LookUp()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            if (WordDB.Find(user, lines) == "")
+            {
+                richTextBox1.Text = "Word not found\nIt either means nothing or hasn't been supported.";
+            }
+            else richTextBox1.Text = textBox1.Text + "\n" + WordDB.Find(user, lines);
+        }
+    }
+}
---
+++ Beta/Dictionary Beta 1/Dictionary Beta 1/WordDatabase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_Beta_1
+{
+    public class WordDatabase
+    {
+        private static WordDatabase instance;
+        internal static WordDatabase Instance
+        {
+            get => instance ?? (instance = new WordDatabase());
+            set => instance = value;
+        }
+
+        public bool IsValid(string line, string user)
+        {
+            string t = "";
+            if (line != "")
+            {
+                if (line[0] == '@')
+                {
+                    for (int i = 1; i < line.Length; i++)
+                    {
+                        if (line[i] == '/')
+                        {
+                            t = line.Substring(1, i - 2);
+                            break;
+                        }
+                    }
+                    if (t == user) return true;
+                }
+            }
+            return false;
+        }
+
+        public string Find(string user, string[] lines)
+        {
+            string result = "";
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (IsValid(lines[i], user))
+                {
+                    int j = i + 1;
+                    while (lines[j] != "")
+                    {
+                        result = result + lines[j] + "\n";
+                        j++;
+                    }
+                }               
+            }
+            return result;
+        }
+    }
+}
