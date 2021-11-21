a07826a27bcdd38026d1f856d8e1b59622a6c6e0
Ecstacyber <duyhao301202@gmail.com>
2021-11-21 23:00:38 +0700

    Backup plan

---
+++ Beta 2.0/Dictionary/Dictionary/MainMenu.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Dictionary
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBoxSearchInput = new System.Windows.Forms.TextBox();
+            this.SearchBar = new System.Windows.Forms.Panel();
+            this.ButtonGo = new System.Windows.Forms.Button();
+            this.PanelSearchBar = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ComboBoxLanguage = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PanelRandomWord = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ButtonSpeak = new System.Windows.Forms.Button();
+            this.SearchBar.SuspendLayout();
+            this.PanelSearchBar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.PanelRandomWord.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBoxSearchInput
+            // 
+            this.TextBoxSearchInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxSearchInput.BackColor = System.Drawing.Color.White;
+            this.TextBoxSearchInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxSearchInput.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxSearchInput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.TextBoxSearchInput.Location = new System.Drawing.Point(25, 15);
+            this.TextBoxSearchInput.Name = "TextBoxSearchInput";
+            this.TextBoxSearchInput.Size = new System.Drawing.Size(315, 20);
+            this.TextBoxSearchInput.TabIndex = 1;
+            this.TextBoxSearchInput.TabStop = false;
+            this.TextBoxSearchInput.Text = "Search";
+            this.TextBoxSearchInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // SearchBar
+            // 
+            this.SearchBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchBar.BackColor = System.Drawing.Color.White;
+            this.SearchBar.Controls.Add(this.TextBoxSearchInput);
+            this.SearchBar.Location = new System.Drawing.Point(223, 15);
+            this.SearchBar.Name = "SearchBar";
+            this.SearchBar.Size = new System.Drawing.Size(350, 48);
+            this.SearchBar.TabIndex = 2;
+            // 
+            // ButtonGo
+            // 
+            this.ButtonGo.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.ButtonGo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(234)))), ((int)(((byte)(118)))));
+            this.ButtonGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonGo.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonGo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ButtonGo.Location = new System.Drawing.Point(569, 15);
+            this.ButtonGo.Name = "ButtonGo";
+            this.ButtonGo.Size = new System.Drawing.Size(48, 48);
+            this.ButtonGo.TabIndex = 2;
+            this.ButtonGo.Text = "Go";
+            this.ButtonGo.UseVisualStyleBackColor = false;
+            this.ButtonGo.Click += new System.EventHandler(this.ButtonGo_Click);
+            // 
+            // PanelSearchBar
+            // 
+            this.PanelSearchBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PanelSearchBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(69)))), ((int)(((byte)(144)))));
+            this.PanelSearchBar.Controls.Add(this.panel1);
+            this.PanelSearchBar.Controls.Add(this.SearchBar);
+            this.PanelSearchBar.Controls.Add(this.ButtonGo);
+            this.PanelSearchBar.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.PanelSearchBar.Location = new System.Drawing.Point(-6, 53);
+            this.PanelSearchBar.Name = "PanelSearchBar";
+            this.PanelSearchBar.Size = new System.Drawing.Size(646, 77);
+            this.PanelSearchBar.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(234)))), ((int)(((byte)(118)))));
+            this.panel1.Controls.Add(this.ComboBoxLanguage);
+            this.panel1.Location = new System.Drawing.Point(26, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 48);
+            this.panel1.TabIndex = 4;
+            // 
+            // ComboBoxLanguage
+            // 
+            this.ComboBoxLanguage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(234)))), ((int)(((byte)(118)))));
+            this.ComboBoxLanguage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ComboBoxLanguage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBoxLanguage.FormattingEnabled = true;
+            this.ComboBoxLanguage.Items.AddRange(new object[] {
+            "English - Vietnamese",
+            "English - English"});
+            this.ComboBoxLanguage.Location = new System.Drawing.Point(16, 11);
+            this.ComboBoxLanguage.Name = "ComboBoxLanguage";
+            this.ComboBoxLanguage.Size = new System.Drawing.Size(175, 24);
+            this.ComboBoxLanguage.TabIndex = 2;
+            this.ComboBoxLanguage.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(118)))), ((int)(((byte)(174)))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(617, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dictionary";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PanelRandomWord
+            // 
+            this.PanelRandomWord.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PanelRandomWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PanelRandomWord.Controls.Add(this.ButtonSpeak);
+            this.PanelRandomWord.Controls.Add(this.label4);
+            this.PanelRandomWord.Controls.Add(this.label3);
+            this.PanelRandomWord.Controls.Add(this.label2);
+            this.PanelRandomWord.Location = new System.Drawing.Point(1, 154);
+            this.PanelRandomWord.Name = "PanelRandomWord";
+            this.PanelRandomWord.Size = new System.Drawing.Size(639, 269);
+            this.PanelRandomWord.TabIndex = 4;
+            this.PanelRandomWord.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelRandomWord_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "WORD YOU MAY WANT TO KNOW";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 36);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "word here";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "/spelling here/";
+            // 
+            // ButtonSpeak
+            // 
+            this.ButtonSpeak.BackgroundImage = global::Dictionary.Properties.Resources.speaker;
+            this.ButtonSpeak.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonSpeak.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonSpeak.Location = new System.Drawing.Point(164, 103);
+            this.ButtonSpeak.Name = "ButtonSpeak";
+            this.ButtonSpeak.Size = new System.Drawing.Size(24, 23);
+            this.ButtonSpeak.TabIndex = 3;
+            this.ButtonSpeak.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(641, 544);
+            this.Controls.Add(this.PanelRandomWord);
+            this.Controls.Add(this.PanelSearchBar);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "MainMenu";
+            this.Text = "Form1";
+            this.SearchBar.ResumeLayout(false);
+            this.SearchBar.PerformLayout();
+            this.PanelSearchBar.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.PanelRandomWord.ResumeLayout(false);
+            this.PanelRandomWord.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TextBoxSearchInput;
+        private System.Windows.Forms.Panel SearchBar;
+        private System.Windows.Forms.Button ButtonGo;
+        private System.Windows.Forms.Panel PanelSearchBar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox ComboBoxLanguage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel PanelRandomWord;
+        private System.Windows.Forms.Button ButtonSpeak;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Beta 2.0/Dictionary/Dictionary/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+            ComboBoxLanguage.SelectionChangeCommitted += ComboBoxLanguage_SelectionChangeCommitted;
+            ButtonSpeak.FlatAppearance.BorderSize = 0;
+            ButtonGo.FlatAppearance.BorderSize = 0;
+            Graphics g = PanelRandomWord.CreateGraphics();
+            ControlPaint.DrawBorder(g, PanelRandomWord.ClientRectangle, PanelSearchBar.BackColor, 4, ButtonBorderStyle.Solid, PanelSearchBar.BackColor, 4, ButtonBorderStyle.Solid, PanelSearchBar.BackColor, 4, ButtonBorderStyle.Solid, PanelSearchBar.BackColor, 4, ButtonBorderStyle.Solid);
+        }
+
+        private void ComboBoxLanguage_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonGo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PanelRandomWord_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, PanelRandomWord.ClientRectangle, Color.DarkBlue, 4, ButtonBorderStyle.Solid, Color.DarkBlue, 4, ButtonBorderStyle.Solid, Color.DarkBlue, 4, ButtonBorderStyle.Solid, Color.DarkBlue, 4, ButtonBorderStyle.Solid);            
+        }
+    }
+}
