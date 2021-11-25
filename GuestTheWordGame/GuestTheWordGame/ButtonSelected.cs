d345a1dfb1f022a5d0d505a4eded9354e319b3da
12Dragon21 <80904153+12Dragon21@users.noreply.github.com>
2021-11-25 22:23:20 +0700

    Minigame

---
+++ GuestTheWordGame/GuestTheWordGame/ButtonSelected.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuestTheWordGame
+{
+    class ButtonSelected : Button
+    {
+        private int iD;
+
+        public int ID { get => iD; set => iD = value; }
+    }
+}
