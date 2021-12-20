e82aba9a97a6f4aafea714c0f592a15ff61b5466
12Dragon21 <80904153+12Dragon21@users.noreply.github.com>
2021-12-20 16:17:24 +0700

    Update GameForm

--- Alpha/Dictionary/DictionaryApp/FormSW.Designer.cs
+++ Alpha/Dictionary/DictionaryApp/FormSW.Designer.cs
@@ -42,17 +42,19 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 173);
+            this.richTextBox1.Location = new System.Drawing.Point(259, 204);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(760, 130);
+            this.richTextBox1.Size = new System.Drawing.Size(1408, 159);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // btn_Submit
             // 
-            this.btn_Submit.Location = new System.Drawing.Point(661, 132);
+            this.btn_Submit.Location = new System.Drawing.Point(1741, 729);
+            this.btn_Submit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Submit.Name = "btn_Submit";
-            this.btn_Submit.Size = new System.Drawing.Size(111, 35);
+            this.btn_Submit.Size = new System.Drawing.Size(148, 43);
             this.btn_Submit.TabIndex = 1;
             this.btn_Submit.Text = "Submit";
             this.btn_Submit.UseVisualStyleBackColor = true;
@@ -61,67 +63,73 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(648, 9);
+            this.label1.Location = new System.Drawing.Point(1797, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(46, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Score:";
             // 
             // lb_Score
             // 
             this.lb_Score.AutoSize = true;
-            this.lb_Score.Location = new System.Drawing.Point(692, 9);
+            this.lb_Score.Location = new System.Drawing.Point(1856, 16);
+            this.lb_Score.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lb_Score.Name = "lb_Score";
-            this.lb_Score.Size = new System.Drawing.Size(13, 13);
+            this.lb_Score.Size = new System.Drawing.Size(14, 16);
             this.lb_Score.TabIndex = 3;
             this.lb_Score.Text = "0";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(634, 39);
+            this.label2.Location = new System.Drawing.Point(1778, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.Size = new System.Drawing.Size(63, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "Question:";
             // 
             // lb_Question
             // 
             this.lb_Question.AutoSize = true;
-            this.lb_Question.Location = new System.Drawing.Point(692, 39);
+            this.lb_Question.Location = new System.Drawing.Point(1856, 53);
+            this.lb_Question.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lb_Question.Name = "lb_Question";
-            this.lb_Question.Size = new System.Drawing.Size(13, 13);
+            this.lb_Question.Size = new System.Drawing.Size(14, 16);
             this.lb_Question.TabIndex = 5;
             this.lb_Question.Text = "1";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(636, 72);
+            this.label3.Location = new System.Drawing.Point(1781, 94);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.Size = new System.Drawing.Size(61, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "Time left:";
             // 
             // lb_TimeLeft
             // 
             this.lb_TimeLeft.AutoSize = true;
-            this.lb_TimeLeft.Location = new System.Drawing.Point(692, 72);
+            this.lb_TimeLeft.Location = new System.Drawing.Point(1856, 94);
+            this.lb_TimeLeft.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lb_TimeLeft.Name = "lb_TimeLeft";
-            this.lb_TimeLeft.Size = new System.Drawing.Size(13, 13);
+            this.lb_TimeLeft.Size = new System.Drawing.Size(14, 16);
             this.lb_TimeLeft.TabIndex = 7;
             this.lb_TimeLeft.Text = "0";
             // 
             // timer1
             // 
-            this.timer1.Interval = 10;
+            this.timer1.Interval = 3;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // FormSW
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
             this.Controls.Add(this.lb_TimeLeft);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lb_Question);
@@ -130,6 +138,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_Submit);
             this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormSW";
             this.Text = "Form2";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
