2fbc9f31ae7c1a289e77df1bc113e393e0806c23
Truongducthien <80139154+Truongducthien@users.noreply.github.com>
2021-12-21 23:34:21 +0700

    add hình

--- Alpha/Dictionary/DictionaryApp/Properties/Resources.Designer.cs
+++ Alpha/Dictionary/DictionaryApp/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace DictionaryApp.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -93,6 +93,16 @@ namespace DictionaryApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap choose {
+            get {
+                object obj = ResourceManager.GetObject("choose", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap close_window_30px {
             get {
                 object obj = ResourceManager.GetObject("close_window_30px", resourceCulture);
@@ -116,6 +126,16 @@ namespace DictionaryApp.Properties {
         internal static System.Drawing.Bitmap game_controller_30px {
             get {
                 object obj = ResourceManager.GetObject("game_controller_30px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hangman_game {
+            get {
+                object obj = ResourceManager.GetObject("hangman_game", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
